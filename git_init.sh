e1253951327f156fe5146fa87211bff06b2340f3
Betterme <313303303@qq.com>
2024-06-21 18:02:09 +0800

    init

--- git_init.sh
+++ git_init.sh
@@ -6,6 +6,7 @@ git add .dockerignore .github/workflows/docker-image.yml
 
 git commit -m "init"
 
+
 #git rm  -r dist
 #git remote add origin git@github.com:yuanjie-ai/MeUtils.git
 #git branch -M master
